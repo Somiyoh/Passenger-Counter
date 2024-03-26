41cd56e59cfc3da145f483bcfd2f67d249599cd6
Somiyo <somiyomagar@gmail.com>
2024-03-25 20:12:57 -0600

    added additional features

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el") // taking arguements
+
+let saveEl = document.getElementById("save-el")
 
 let count = 0
 // function that increments on click
@@ -9,6 +11,7 @@ function increment() {
     countEl.innerText = count
 }
 
+// fucnction that decrements on click
 function decrement() {
     if(count < 1) {
         count = 0
@@ -18,6 +21,21 @@ function decrement() {
         countEl.innerText = count
     }  
 }
+
+// fucntion to save entries 
+function save() {
+    let counStr = count + " - "
+    saveEl.textContent += counStr
+    makeCountZero();
+
+}
+
+// everytime they hit save, we need to reset the counter to 0 so we can count again
+function makeCountZero() {
+    count = 0
+    countEl.innerText = count
+}
+
 
 
 
